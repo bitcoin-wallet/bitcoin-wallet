8ea6fee79121ef8b7176910341e739ffd3b0cd30
Andreas Schildbach <andreas@schildbach.de>
2011-06-13 15:34:13 +0000

    alternative source for exchange rate
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@56 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/WalletActivity.java
+++ src/de/schildbach/wallet/WalletActivity.java
@@ -531,7 +531,7 @@ public class WalletActivity extends Activity
 	{
 		try
 		{
-			final URLConnection connection = new URL("https://mtgox.com/code/data/ticker.php").openConnection();
+			final URLConnection connection = new URL("https://mtgox.com/code/data/ticker.php").openConnection(); // https://bitmarket.eu/api/ticker
 			connection.connect();
 			final Reader is = new InputStreamReader(new BufferedInputStream(connection.getInputStream()));
 			final StringBuilder content = new StringBuilder();
