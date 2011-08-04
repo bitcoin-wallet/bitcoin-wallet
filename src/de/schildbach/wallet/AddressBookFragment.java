89207da27227f563ad0fc0d486b5cabc07aa73e3
Andreas Schildbach <andreas@schildbach.de>
2011-08-04 10:26:00 +0000

    remove and edit addresses from address book
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@331 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/AddressBookFragment.java
+++ src/de/schildbach/wallet/AddressBookFragment.java
@@ -19,14 +19,22 @@ package de.schildbach.wallet;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 import android.widget.TextView;
 import de.schildbach.wallet_test.R;
@@ -65,14 +73,75 @@ public class AddressBookFragment extends ListFragment implements LoaderManager.L
 	}
 
 	@Override
+	public void onViewCreated(final View view, final Bundle savedInstanceState)
+	{
+		super.onViewCreated(view, savedInstanceState);
+
+		registerForContextMenu(getListView());
+	}
+
+	@Override
 	public void onListItemClick(final ListView l, final View v, final int position, final long id)
 	{
-		final Cursor cursor = adapter.getCursor();
+		final Cursor cursor = (Cursor) adapter.getItem(position);
+		final String address = cursor.getString(cursor.getColumnIndexOrThrow(AddressBookProvider.KEY_ADDRESS));
+		handleSend(address);
+	}
+
+	@Override
+	public void onCreateContextMenu(final ContextMenu menu, final View v, final ContextMenuInfo menuInfo)
+	{
+		getActivity().getMenuInflater().inflate(R.menu.address_book_context, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(final MenuItem item)
+	{
+		final AdapterContextMenuInfo menuInfo = (AdapterContextMenuInfo) item.getMenuInfo();
+		final Cursor cursor = (Cursor) adapter.getItem(menuInfo.position);
 		final String address = cursor.getString(cursor.getColumnIndexOrThrow(AddressBookProvider.KEY_ADDRESS));
 
+		switch (item.getItemId())
+		{
+			case R.id.address_book_context_send:
+				handleSend(address);
+				return true;
+
+			case R.id.address_book_context_edit:
+				handleEdit(address);
+				return true;
+
+			case R.id.address_book_context_remove:
+				handleRemove(address);
+				return true;
+
+			default:
+				return false;
+		}
+	}
+
+	private void handleSend(final String address)
+	{
 		final Intent intent = new Intent(getActivity(), SendCoinsActivity.class);
 		intent.putExtra(SendCoinsActivity.INTENT_EXTRA_ADDRESS, address);
 		startActivity(intent);
+	}
+
+	private void handleEdit(final String address)
+	{
+		final FragmentTransaction ft = getFragmentManager().beginTransaction();
+		final Fragment prev = getFragmentManager().findFragmentByTag(EditAddressBookEntryFragment.FRAGMENT_TAG);
+		if (prev != null)
+			ft.remove(prev);
+		ft.addToBackStack(null);
+		final DialogFragment newFragment = new EditAddressBookEntryFragment(getLayoutInflater(null), address);
+		newFragment.show(ft, EditAddressBookEntryFragment.FRAGMENT_TAG);
+	}
+
+	private void handleRemove(final String address)
+	{
+		final Uri uri = AddressBookProvider.CONTENT_URI.buildUpon().appendPath(address).build();
+		getActivity().getContentResolver().delete(uri, null, null);
 	}
 
 	public Loader<Cursor> onCreateLoader(final int id, final Bundle args)
