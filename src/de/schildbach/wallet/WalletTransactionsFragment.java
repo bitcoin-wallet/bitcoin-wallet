3099f26920d89cb18a3551b85331901251c83239
Andreas Schildbach <andreas@schildbach.de>
2011-08-05 20:06:15 +0000

    more debug
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@343 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/WalletTransactionsFragment.java
+++ src/de/schildbach/wallet/WalletTransactionsFragment.java
@@ -184,7 +184,7 @@ public class WalletTransactionsFragment extends Fragment
 					final boolean sent = tx.sent(application.getWallet());
 					final Address address = sent ? tx.outputs.get(0).getScriptPubKey().getToAddress() : tx.getInputs().get(0).getFromAddress();
 
-					System.out.println("clicked on tx " + tx.getHash());
+					System.out.println("clicked on tx " + tx);
 
 					final FragmentTransaction ft = getFragmentManager().beginTransaction();
 					final Fragment prev = getFragmentManager().findFragmentByTag(EditAddressBookEntryFragment.FRAGMENT_TAG);
