d161d4792a60201c1fc695f8e335866a8b84a97e
Andreas Schildbach <andreas@schildbach.de>
2011-08-16 16:04:09 +0000

    a little synchronization on the error reporter
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@375 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/ErrorReporter.java
+++ src/de/schildbach/wallet/ErrorReporter.java
@@ -69,7 +69,7 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler
 		return instance;
 	}
 
-	public void init(final Context context)
+	public synchronized void init(final Context context)
 	{
 		previousHandler = Thread.getDefaultUncaughtExceptionHandler();
 		Thread.setDefaultUncaughtExceptionHandler(this);
@@ -145,7 +145,7 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler
 		return totalBlocks * blockSize;
 	}
 
-	public void uncaughtException(final Thread t, final Throwable e)
+	public synchronized void uncaughtException(final Thread t, final Throwable e)
 	{
 		report.append("=== collected at exception time ===\n\n");
 
@@ -271,7 +271,7 @@ public class ErrorReporter implements Thread.UncaughtExceptionHandler
 				appendReport(report, f, indent + 1);
 	}
 
-	public void check(final Context context)
+	public synchronized void check(final Context context)
 	{
 		if (!stackTraceFile.exists())
 			return;
