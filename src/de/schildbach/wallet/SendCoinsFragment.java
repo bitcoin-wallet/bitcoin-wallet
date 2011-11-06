57633dadba3339a4566523612e1ffc1df819f79f
Andreas Schildbach <andreas@schildbach.de>
2011-11-06 18:11:14 +0000

    fixed NPE
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@493 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/SendCoinsFragment.java
+++ src/de/schildbach/wallet/SendCoinsFragment.java
@@ -238,7 +238,7 @@ public class SendCoinsFragment extends Fragment
 
 								final Uri uri = AddressBookProvider.CONTENT_URI.buildUpon().appendPath(receivingAddress.toString()).build();
 								final Cursor cursor = activity.managedQuery(uri, null, null, null, null);
-								if (cursor.getCount() == 0)
+								if (cursor != null && cursor.getCount() == 0)
 									showAddAddressDialog(receivingAddress.toString());
 							}
 						};
