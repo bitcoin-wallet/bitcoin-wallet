62154335edcc3c73bdf4c158ab414e6db69213f1
Andreas Schildbach <andreas@schildbach.de>
2012-02-11 23:33:21 +0000

    autosync
    git-svn-id: https://bitcoin-wallet.googlecode.com/svn/trunk@664 06f53a37-7512-94ec-bf3d-2939b97928a9

--- src/de/schildbach/wallet/Constants.java
+++ src/de/schildbach/wallet/Constants.java
@@ -106,6 +106,7 @@ public class Constants
 	public static final String PACKAGE_NAME_ZXING = "com.google.zxing.client.android";
 
 	public static final String PREFS_KEY_LAST_VERSION = "last_version";
+	public static final String PREFS_KEY_AUTOSYNC = "autosync";
 	public static final String PREFS_KEY_SELECTED_ADDRESS = "selected_address";
 	public static final String PREFS_KEY_EXCHANGE_CURRENCY = "exchange_currency";
 	public static final String PREFS_KEY_TRUSTED_PEER = "trusted_peer";
---
+++ src/de/schildbach/wallet/service/AutosyncReceiver.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.schildbach.wallet.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import de.schildbach.wallet.Constants;
+
+/**
+ * @author Andreas Schildbach
+ */
+public class AutosyncReceiver extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(final Context context, final Intent intent)
+	{
+		final String action = intent.getAction();
+
+		if (Intent.ACTION_POWER_CONNECTED.equals(action))
+		{
+			final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+			final boolean autosync = prefs.getBoolean(Constants.PREFS_KEY_AUTOSYNC, false);
+
+			if (autosync)
+				context.startService(new Intent(context, Service.class));
+		}
+		else if (Intent.ACTION_POWER_DISCONNECTED.equals(action))
+		{
+			context.stopService(new Intent(context, Service.class));
+		}
+	}
+}
