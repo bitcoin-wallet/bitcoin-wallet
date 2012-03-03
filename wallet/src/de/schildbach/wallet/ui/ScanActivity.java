5481df76d19a6b9672bf88ce75f0416b0455f6fe
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version.

--- wallet/src/de/schildbach/wallet/ui/ScanActivity.java
+++ wallet/src/de/schildbach/wallet/ui/ScanActivity.java
@@ -34,7 +34,7 @@ import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
 import de.schildbach.wallet.camera.CameraManager;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.Manifest;
 import android.app.Activity;
--- wallet/src/de/schildbach/wallet/ui/send/RequestPaymentRequestTask.java
+++ wallet/src/de/schildbach/wallet/ui/send/RequestPaymentRequestTask.java
@@ -38,7 +38,7 @@ import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.data.PaymentIntent;
 import de.schildbach.wallet.ui.InputParser;
 import de.schildbach.wallet.util.Bluetooth;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
