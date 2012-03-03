97f0e7e33341d01c9d8db8290facd4462d7c22a6
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version

--- wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletTransactionsFragment.java
@@ -27,7 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import de.schildbach.wallet.ui.TransactionsListFragment.Direction;
 import de.schildbach.wallet.util.ViewPagerTabs;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 /**
  * @author Andreas Schildbach
