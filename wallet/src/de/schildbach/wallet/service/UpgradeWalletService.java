b0813c9889c5e91edf77415417605f1fb51d7431
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version.

--- wallet/src/de/schildbach/wallet/service/UpgradeWalletService.java
+++ wallet/src/de/schildbach/wallet/service/UpgradeWalletService.java
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory;
 
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.WalletApplication;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.app.IntentService;
 import android.content.Context;
--- wallet/src/de/schildbach/wallet/ui/preference/SettingsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/preference/SettingsFragment.java
@@ -25,7 +25,7 @@ import org.slf4j.LoggerFactory;
 import de.schildbach.wallet.Configuration;
 import de.schildbach.wallet.WalletApplication;
 import de.schildbach.wallet.WalletBalanceWidgetProvider;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.app.Activity;
 import android.content.pm.PackageManager;
