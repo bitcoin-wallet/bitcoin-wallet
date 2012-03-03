294f8108160f3e0d4b741731cef10cd583a4f780
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version.

--- wallet/src/de/schildbach/wallet/ui/CurrencyAmountView.java
+++ wallet/src/de/schildbach/wallet/ui/CurrencyAmountView.java
@@ -26,7 +26,7 @@ import org.bitcoinj.utils.MonetaryFormat;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.util.GenericUtils;
 import de.schildbach.wallet.util.MonetarySpannable;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.content.Context;
 import android.content.res.Resources;
--- wallet/src/de/schildbach/wallet/ui/preference/AboutFragment.java
+++ wallet/src/de/schildbach/wallet/ui/preference/AboutFragment.java
@@ -21,7 +21,7 @@ import org.bitcoinj.core.VersionMessage;
 
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.WalletApplication;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.app.Activity;
 import android.content.Intent;
