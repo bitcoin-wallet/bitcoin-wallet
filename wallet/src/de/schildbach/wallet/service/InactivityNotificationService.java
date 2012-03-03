a8007a441674c2ad18dbc9550807a0e95808c623
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version.

--- wallet/src/de/schildbach/wallet/service/InactivityNotificationService.java
+++ wallet/src/de/schildbach/wallet/service/InactivityNotificationService.java
@@ -30,7 +30,7 @@ import de.schildbach.wallet.WalletApplication;
 import de.schildbach.wallet.ui.WalletActivity;
 import de.schildbach.wallet.ui.send.FeeCategory;
 import de.schildbach.wallet.ui.send.SendCoinsActivity;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
--- wallet/src/de/schildbach/wallet/ui/send/SendCoinsActivity.java
+++ wallet/src/de/schildbach/wallet/ui/send/SendCoinsActivity.java
@@ -25,7 +25,7 @@ import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.data.PaymentIntent;
 import de.schildbach.wallet.ui.AbstractBindServiceActivity;
 import de.schildbach.wallet.ui.HelpDialogFragment;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 import android.content.Context;
 import android.content.Intent;
