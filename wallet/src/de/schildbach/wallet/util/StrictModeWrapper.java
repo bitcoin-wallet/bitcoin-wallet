9c6c7072ed3c8a0e8fc2b8407ecb153b58ae116c
Andreas Schildbach <andreas@schildbach.de>
2012-04-21 17:38:19 +0200

    fixed lint errors

--- wallet/src/de/schildbach/wallet/util/StrictModeWrapper.java
+++ wallet/src/de/schildbach/wallet/util/StrictModeWrapper.java
@@ -17,11 +17,13 @@
 
 package de.schildbach.wallet.util;
 
+import android.annotation.TargetApi;
 import android.os.StrictMode;
 
 /**
  * @author Andreas Schildbach
  */
+@TargetApi(9)
 public class StrictModeWrapper
 {
 	public static void init()
