42219f6d370ebe869bafafd4a22875566341fa63
Andreas Schildbach <andreas@schildbach.de>
2013-06-21 00:00:04 +0200

    organize imports

--- wallet/src/de/schildbach/wallet/WalletApplication.java
+++ wallet/src/de/schildbach/wallet/WalletApplication.java
@@ -20,7 +20,6 @@ package de.schildbach.wallet;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
--- wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
@@ -24,7 +24,6 @@ import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
