075cd6352473612ec29d7a1bf7e0d05fc9073e28
Andreas Schildbach <andreas@schildbach.de>
2013-07-10 09:48:08 +0200

    Don't append index of cache directory to reports

--- wallet/src/de/schildbach/wallet/util/CrashReporter.java
+++ wallet/src/de/schildbach/wallet/util/CrashReporter.java
@@ -213,9 +213,6 @@ public class CrashReporter
 			final File filesDir = application.getFilesDir();
 			report.append("\nContents of FilesDir " + filesDir + ":\n");
 			appendDir(report, filesDir, 0);
-			final File cacheDir = application.getCacheDir();
-			report.append("\nContents of CacheDir " + cacheDir + ":\n");
-			appendDir(report, cacheDir, 0);
 			final File logDir = application.getDir("log", Context.MODE_PRIVATE);
 			report.append("\nContents of LogDir " + logDir + ":\n");
 			appendDir(report, logDir, 0);
