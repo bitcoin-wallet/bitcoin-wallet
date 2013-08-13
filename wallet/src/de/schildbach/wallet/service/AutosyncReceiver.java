af16a68634b95c50efb41cc43fe8e1383f539e2a
Andreas Schildbach <andreas@schildbach.de>
2013-08-13 18:29:39 +0200

    Don't trigger blockchain sync because of plugging power

--- wallet/src/de/schildbach/wallet/service/AutosyncReceiver.java
+++ wallet/src/de/schildbach/wallet/service/AutosyncReceiver.java
@@ -25,9 +25,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.BatteryManager;
 import android.preference.PreferenceManager;
 import de.schildbach.wallet.Constants;
 
@@ -50,36 +48,23 @@ public class AutosyncReceiver extends BroadcastReceiver
 		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		final long prefsLastUsed = prefs.getLong(Constants.PREFS_KEY_LAST_USED, 0);
 
-		// determine power connected state
-		final Intent batteryChanged = context.getApplicationContext().registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-		final int batteryStatus = batteryChanged.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-		boolean isPowerConnected = batteryStatus == BatteryManager.BATTERY_STATUS_CHARGING || batteryStatus == BatteryManager.BATTERY_STATUS_FULL;
-
 		final AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
 		final Intent serviceIntent = new Intent(BlockchainService.ACTION_HOLD_WIFI_LOCK, null, context, BlockchainServiceImpl.class);
-		final PendingIntent alarmIntent = PendingIntent.getService(context, 0, serviceIntent, 0);
+		context.startService(serviceIntent);
 
-		if (isPowerConnected)
-		{
-			context.startService(serviceIntent);
+		final long now = System.currentTimeMillis();
 
-			final long now = System.currentTimeMillis();
-
-			final long lastUsedAgo = now - prefsLastUsed;
-			final long alarmInterval;
-			if (lastUsedAgo < Constants.LAST_USAGE_THRESHOLD_JUST_MS)
-				alarmInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
-			else if (lastUsedAgo < Constants.LAST_USAGE_THRESHOLD_RECENTLY_MS)
-				alarmInterval = AlarmManager.INTERVAL_HOUR;
-			else
-				alarmInterval = AlarmManager.INTERVAL_HALF_DAY;
-
-			alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, now, alarmInterval, alarmIntent);
-		}
+		final long lastUsedAgo = now - prefsLastUsed;
+		final long alarmInterval;
+		if (lastUsedAgo < Constants.LAST_USAGE_THRESHOLD_JUST_MS)
+			alarmInterval = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+		else if (lastUsedAgo < Constants.LAST_USAGE_THRESHOLD_RECENTLY_MS)
+			alarmInterval = AlarmManager.INTERVAL_HOUR;
 		else
-		{
-			alarmManager.cancel(alarmIntent);
-		}
+			alarmInterval = AlarmManager.INTERVAL_HALF_DAY;
+
+		final PendingIntent alarmIntent = PendingIntent.getService(context, 0, serviceIntent, 0);
+		alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, now, alarmInterval, alarmIntent);
 	}
 }
