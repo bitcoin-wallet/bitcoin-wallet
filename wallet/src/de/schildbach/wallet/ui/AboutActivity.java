9467d8291c283c790b8f495deb208af7f5b2a2b9
Andreas Schildbach <andreas@schildbach.de>
2013-12-02 09:45:20 +0100

    Reorder items in about screen

--- wallet/src/de/schildbach/wallet/ui/AboutActivity.java
+++ wallet/src/de/schildbach/wallet/ui/AboutActivity.java
@@ -40,14 +40,14 @@ public final class AboutActivity extends SherlockPreferenceActivity
 	private static final String KEY_ABOUT_VERSION = "about_version";
 	private static final String KEY_ABOUT_LICENSE = "about_license";
 	private static final String KEY_ABOUT_SOURCE = "about_source";
-	private static final String KEY_ABOUT_CREDITS_BITCOINJ = "about_credits_bitcoinj";
-	private static final String KEY_ABOUT_CREDITS_ZXING = "about_credits_zxing";
-	private static final String KEY_ABOUT_CREDITS_ICON = "about_credits_icon";
+	private static final String KEY_ABOUT_MARKET_APP = "about_market_app";
 	private static final String KEY_ABOUT_COMMUNITY_GOOGLEPLUS = "about_community_googleplus";
 	private static final String KEY_ABOUT_AUTHOR_TWITTER = "about_author_twitter";
 	private static final String KEY_ABOUT_AUTHOR_GOOGLEPLUS = "about_author_googleplus";
-	private static final String KEY_ABOUT_MARKET_APP = "about_market_app";
 	private static final String KEY_ABOUT_MARKET_PUBLISHER = "about_market_publisher";
+	private static final String KEY_ABOUT_CREDITS_BITCOINJ = "about_credits_bitcoinj";
+	private static final String KEY_ABOUT_CREDITS_ZXING = "about_credits_zxing";
+	private static final String KEY_ABOUT_CREDITS_ICON = "about_credits_icon";
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState)
@@ -62,12 +62,12 @@ public final class AboutActivity extends SherlockPreferenceActivity
 		findPreference(KEY_ABOUT_VERSION).setSummary(((WalletApplication) getApplication()).packageInfo().versionName);
 		findPreference(KEY_ABOUT_LICENSE).setSummary(Constants.LICENSE_URL);
 		findPreference(KEY_ABOUT_SOURCE).setSummary(Constants.SOURCE_URL);
+		findPreference(KEY_ABOUT_MARKET_APP).setSummary(String.format(Constants.MARKET_APP_URL, getPackageName()));
+		findPreference(KEY_ABOUT_MARKET_PUBLISHER).setSummary(Constants.MARKET_PUBLISHER_URL);
 		findPreference(KEY_ABOUT_CREDITS_BITCOINJ).setTitle(getString(R.string.about_credits_bitcoinj_title, VersionMessage.BITCOINJ_VERSION));
 		findPreference(KEY_ABOUT_CREDITS_BITCOINJ).setSummary(Constants.CREDITS_BITCOINJ_URL);
 		findPreference(KEY_ABOUT_CREDITS_ZXING).setSummary(Constants.CREDITS_ZXING_URL);
 		findPreference(KEY_ABOUT_CREDITS_ICON).setSummary(Constants.CREDITS_ICON_URL);
-		findPreference(KEY_ABOUT_MARKET_APP).setSummary(String.format(Constants.MARKET_APP_URL, getPackageName()));
-		findPreference(KEY_ABOUT_MARKET_PUBLISHER).setSummary(Constants.MARKET_PUBLISHER_URL);
 	}
 
 	@Override
@@ -97,19 +97,13 @@ public final class AboutActivity extends SherlockPreferenceActivity
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.SOURCE_URL)));
 			finish();
 		}
-		else if (KEY_ABOUT_CREDITS_BITCOINJ.equals(key))
+		else if (KEY_ABOUT_MARKET_APP.equals(key))
 		{
-			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_BITCOINJ_URL)));
-			finish();
-		}
-		else if (KEY_ABOUT_CREDITS_ZXING.equals(key))
-		{
-			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_ZXING_URL)));
-			finish();
-		}
-		else if (KEY_ABOUT_CREDITS_ICON.equals(key))
-		{
-			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_ICON_URL)));
+			final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.MARKET_APP_URL, getPackageName())));
+			if (getPackageManager().resolveActivity(marketIntent, 0) != null)
+				startActivity(marketIntent);
+			else
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.WEBMARKET_APP_URL, getPackageName()))));
 			finish();
 		}
 		else if (KEY_ABOUT_COMMUNITY_GOOGLEPLUS.equals(key))
@@ -127,18 +121,24 @@ public final class AboutActivity extends SherlockPreferenceActivity
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.AUTHOR_GOOGLEPLUS_URL)));
 			finish();
 		}
-		else if (KEY_ABOUT_MARKET_APP.equals(key))
-		{
-			final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.MARKET_APP_URL, getPackageName())));
-			if (getPackageManager().resolveActivity(marketIntent, 0) != null)
-				startActivity(marketIntent);
-			else
-				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(Constants.WEBMARKET_APP_URL, getPackageName()))));
-			finish();
-		}
 		else if (KEY_ABOUT_MARKET_PUBLISHER.equals(key))
 		{
 			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.MARKET_PUBLISHER_URL)));
+			finish();
+		}
+		else if (KEY_ABOUT_CREDITS_BITCOINJ.equals(key))
+		{
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_BITCOINJ_URL)));
+			finish();
+		}
+		else if (KEY_ABOUT_CREDITS_ZXING.equals(key))
+		{
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_ZXING_URL)));
+			finish();
+		}
+		else if (KEY_ABOUT_CREDITS_ICON.equals(key))
+		{
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CREDITS_ICON_URL)));
 			finish();
 		}
 
