be908b58d9bc4600a817bc7822e53a101f0af881
langerhans <max.keller@gmx.com>
2013-12-29 21:17:25 +0100

    3 Conf. are enough for now. Also, default show peer indicator to make users see some progress.

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -59,7 +59,7 @@ public class Constants
 
 	public static final String MIMETYPE_TRANSACTION = "application/x-btctx";
 
-	public static final int MAX_NUM_CONFIRMATIONS = 7;
+	public static final int MAX_NUM_CONFIRMATIONS = 3;
 	public static final String USER_AGENT = "Bitcoin Wallet";
 	public static final String DEFAULT_EXCHANGE_CURRENCY = "USD";
 	public static final int WALLET_OPERATION_STACK_SIZE = 256 * 1024;
