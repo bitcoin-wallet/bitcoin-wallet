6a8b6cc6c3ce249e7008e9df060f7d4c295602d7
langerhans <max.keller@gmx.com>
2013-12-31 14:32:20 +0100

    Connectivity notification is defaul on

--- wallet/src/de/langerhans/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/de/langerhans/wallet/service/BlockchainServiceImpl.java
@@ -279,7 +279,7 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
 				@Override
 				public void run()
 				{
-					final boolean connectivityNotification = prefs.getBoolean(Constants.PREFS_KEY_CONNECTIVITY_NOTIFICATION, false);
+					final boolean connectivityNotification = prefs.getBoolean(Constants.PREFS_KEY_CONNECTIVITY_NOTIFICATION, true);
 
 					if (!connectivityNotification || numPeers == 0)
 					{
