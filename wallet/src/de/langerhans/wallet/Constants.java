fde415b7b05bf2314edcab163bf9d5a68af7ee9a
langerhans <max.keller@gmx.com>
2014-01-01 19:48:39 +0100

    Change mail

--- wallet/src/de/langerhans/wallet/Constants.java
+++ wallet/src/de/langerhans/wallet/Constants.java
@@ -80,7 +80,7 @@ public class Constants
 	public static final int LOCAL_PRECISION = 4;
 
 	public static final String DONATION_ADDRESS = "DEwTHxztkSDUJEMywbPk5zVUGrupaNRqEy";
-	public static final String REPORT_EMAIL = "max.keller@gmx.com";
+	public static final String REPORT_EMAIL = "dogecoinandroid@gmail.com";
 	public static final String REPORT_SUBJECT_ISSUE = "Reported issue";
 	public static final String REPORT_SUBJECT_CRASH = "Crash report";
 
