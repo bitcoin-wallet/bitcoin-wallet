719be185bfb9d5b7c1bbd6f7c7955279bea03088
Andreas Schildbach <andreas@schildbach.de>
2014-01-04 15:10:36 +0100

    Require Android 4.3 for requesting Bitcoins via Bluetooth (because of a permission bug)

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -123,6 +123,7 @@ public class Constants
 	public static final long LAST_USAGE_THRESHOLD_RECENTLY_MS = 2 * DateUtils.DAY_IN_MILLIS;
 
 	public static final int SDK_JELLY_BEAN = 16;
+	public static final int SDK_JELLY_BEAN_MR2 = 18;
 
 	public static final int MEMORY_CLASS_LOWEND = 48;
 
