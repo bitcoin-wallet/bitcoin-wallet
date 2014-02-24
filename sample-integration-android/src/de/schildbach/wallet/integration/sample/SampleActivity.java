a22e63e7df61e93d1a90b3beab1cdffc0c96c898
Andreas Schildbach <andreas@schildbach.de>
2014-02-24 13:54:26 +0100

    Extend sample implementation to usage of the payment protocol for in-app payments.

--- sample-integration-android/src/de/schildbach/wallet/integration/sample/SampleActivity.java
+++ sample-integration-android/src/de/schildbach/wallet/integration/sample/SampleActivity.java
@@ -16,6 +16,8 @@
 
 package de.schildbach.wallet.integration.sample;
 
+import org.bitcoin.protocols.payments.Protos;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -27,6 +29,13 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.AddressFormatException;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.script.ScriptBuilder;
+import com.google.protobuf.ByteString;
+
 import de.schildbach.wallet.integration.android.BitcoinIntegration;
 
 /**
@@ -34,11 +43,15 @@ import de.schildbach.wallet.integration.android.BitcoinIntegration;
  */
 public class SampleActivity extends Activity
 {
-	private static final String DONATION_ADDRESS = "1PZmMahjbfsTy6DsaRyfStzoWTPppWwDnZ"; // prodnet
-	// private static final String DONATION_ADDRESS = "mwEacn7pYszzxfgcNaVUzYvzL6ypRJzB6A"; // testnet
+	private static final long AMOUNT = 500000;
+	private static final String DONATION_ADDRESS = "18CK5k1gajRKKSC7yVSTXT9LUzbheh1XY4"; // mainnet
+	private static final String DONATION_ADDRESS2 = "1PZmMahjbfsTy6DsaRyfStzoWTPppWwDnZ"; // mainnet
+	// private static final String DONATION_ADDRESS = "mkCLjaXncyw8eSWJBcBtnTgviU85z5PfwS"; // testnet
+	// private static final String DONATION_ADDRESS2 = "mwEacn7pYszzxfgcNaVUzYvzL6ypRJzB6A"; // testnet
+	private static final String MEMO = "Sample donation";
 	private static final int REQUEST_CODE = 0;
 
-	private Button donateButton;
+	private Button donateButton, requestButton;
 	private TextView donateMessage;
 
 	@Override
@@ -49,16 +62,61 @@ public class SampleActivity extends Activity
 		setContentView(R.layout.sample_activity);
 
 		donateButton = (Button) findViewById(R.id.sample_donate_button);
-
 		donateButton.setOnClickListener(new OnClickListener()
 		{
 			public void onClick(final View v)
 			{
-				BitcoinIntegration.requestForResult(SampleActivity.this, REQUEST_CODE, DONATION_ADDRESS);
+				handleDonate();
+			}
+		});
+
+		requestButton = (Button) findViewById(R.id.sample_request_button);
+		requestButton.setOnClickListener(new OnClickListener()
+		{
+			public void onClick(final View v)
+			{
+				handleRequest();
 			}
 		});
 
 		donateMessage = (TextView) findViewById(R.id.sample_donate_message);
+	}
+
+	private void handleDonate()
+	{
+		BitcoinIntegration.requestForResult(SampleActivity.this, REQUEST_CODE, DONATION_ADDRESS);
+	}
+
+	private void handleRequest()
+	{
+		try
+		{
+			final NetworkParameters params = Address.getParametersFromAddress(DONATION_ADDRESS);
+
+			final Protos.Output.Builder output1 = Protos.Output.newBuilder();
+			output1.setAmount(AMOUNT);
+			output1.setScript(ByteString.copyFrom(ScriptBuilder.createOutputScript(new Address(params, DONATION_ADDRESS)).getProgram()));
+
+			final Protos.Output.Builder output2 = Protos.Output.newBuilder();
+			output2.setAmount(AMOUNT);
+			output2.setScript(ByteString.copyFrom(ScriptBuilder.createOutputScript(new Address(params, DONATION_ADDRESS2)).getProgram()));
+
+			final Protos.PaymentDetails.Builder paymentDetails = Protos.PaymentDetails.newBuilder();
+			paymentDetails.setNetwork(params.getPaymentProtocolId());
+			paymentDetails.addOutputs(output1);
+			paymentDetails.addOutputs(output2);
+			paymentDetails.setMemo(MEMO);
+			paymentDetails.setTime(System.currentTimeMillis());
+
+			final Protos.PaymentRequest.Builder paymentRequest = Protos.PaymentRequest.newBuilder();
+			paymentRequest.setSerializedPaymentDetails(paymentDetails.build().toByteString());
+
+			BitcoinIntegration.requestForResult(SampleActivity.this, REQUEST_CODE, paymentRequest.build().toByteArray());
+		}
+		catch (final AddressFormatException x)
+		{
+			throw new RuntimeException(x);
+		}
 	}
 
 	@Override
@@ -76,12 +134,12 @@ public class SampleActivity extends Activity
 					messageBuilder.setSpan(new TypefaceSpan("monospace"), messageBuilder.length() - txHash.length(), messageBuilder.length(),
 							Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+					if (BitcoinIntegration.paymentFromResult(data) != null)
+						messageBuilder.append("\n(also a BIP70 payment message was received)");
+
 					donateMessage.setText(messageBuilder);
 					donateMessage.setVisibility(View.VISIBLE);
 				}
-
-				donateButton.setEnabled(false);
-				donateButton.setText("Already donated");
 
 				Toast.makeText(this, "Thank you!", Toast.LENGTH_LONG).show();
 			}
