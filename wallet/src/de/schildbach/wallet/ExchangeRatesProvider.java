43288c25a72b1d944f8c1edb87754d08bb67cd92
HashEngineering <hashengineeringsolutions@gmail.com>
2014-03-17 22:16:58 -0700

    Fix a bug on exchange rates with , and .

--- wallet/src/de/schildbach/wallet/ExchangeRatesProvider.java
+++ wallet/src/de/schildbach/wallet/ExchangeRatesProvider.java
@@ -453,7 +453,7 @@ public class ExchangeRatesProvider extends ContentProvider
 								{
                                     double rateForBTC = Double.parseDouble(rateStr);
 
-                                    rateStr = String.format("%.8f", rateForBTC * btcRate);
+                                    rateStr = String.format("%.8f", rateForBTC * btcRate).replace(",", ".");
 
 									final BigInteger rate = GenericUtils.toNanoCoins(rateStr, 0);
 
