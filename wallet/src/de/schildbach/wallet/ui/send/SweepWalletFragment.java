38dc85bd31db2fbc9e00f6a384b2a97d04587c47
Andreas Schildbach <andreas@schildbach.de>
2014-11-25 16:56:01 +0100

    Fix some license headers.

--- wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.schildbach.wallet.ui.send;
 
 import java.util.Collection;
@@ -59,7 +76,6 @@ import de.schildbach.wallet.util.WalletUtils;
 import de.schildbach.wallet_test.R;
 
 /**
- * @author Maximilian Keller
  * @author Andreas Schildbach
  */
 public class SweepWalletFragment extends Fragment
@@ -140,7 +156,7 @@ public class SweepWalletFragment extends Fragment
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
 	{
 		final View view = inflater.inflate(R.layout.sweep_wallet_fragment, container);
 
@@ -393,7 +409,7 @@ public class SweepWalletFragment extends Fragment
 			}
 
 			@Override
-			public void onFail(int messageResId, Object... messageArgs)
+			public void onFail(final int messageResId, final Object... messageArgs)
 			{
 				ProgressDialogFragment.dismissProgress(fragmentManager);
 
