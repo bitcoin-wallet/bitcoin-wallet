01f6ebd3638e49a113977b3f3a7956312dde1d89
Andreas Schildbach <andreas@schildbach.de>
2015-01-15 13:39:17 +0100

    Disable 'move' button in maintenance dialog until PIN is entered.

--- wallet/src/de/schildbach/wallet/ui/send/MaintenanceDialogFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/MaintenanceDialogFragment.java
@@ -314,12 +314,13 @@ public class MaintenanceDialogFragment extends DialogFragment
 		if (dialog == null)
 			return;
 
-		passwordGroup.setVisibility(wallet.isEncrypted() ? View.VISIBLE : View.GONE);
+		final boolean needsPassword = wallet.isEncrypted();
+		passwordGroup.setVisibility(needsPassword ? View.VISIBLE : View.GONE);
 
 		if (state == State.INPUT)
 		{
 			positiveButton.setText(R.string.maintenance_dialog_button_move);
-			positiveButton.setEnabled(true);
+			positiveButton.setEnabled(!needsPassword || passwordView.getText().toString().trim().length() > 0);
 			negativeButton.setEnabled(true);
 		}
 		else if (state == State.DECRYPTING)
