81e9f7eea3b54fe71099392586a3838e1ddab1d5
Andreas Schildbach <andreas@schildbach.de>
2015-06-16 23:10:36 +0200

    Adjust fee per KB for the normal fee category.

--- wallet/src/de/schildbach/wallet/ui/send/SendCoinsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/SendCoinsFragment.java
@@ -1064,7 +1064,7 @@ public final class SendCoinsFragment extends Fragment
 		if (feeCategory == FeeCategory.ECONOMIC)
 			return SendRequest.DEFAULT_FEE_PER_KB;
 		else if (feeCategory == FeeCategory.NORMAL)
-			return SendRequest.DEFAULT_FEE_PER_KB.multiply(2);
+			return SendRequest.DEFAULT_FEE_PER_KB.multiply(5);
 		else if (feeCategory == FeeCategory.PRIORITY)
 			return SendRequest.DEFAULT_FEE_PER_KB.multiply(10);
 		else
