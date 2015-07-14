7c9248a887438d4d4350cc251ec386292e9a7c03
Andreas Schildbach <andreas@schildbach.de>
2015-07-14 09:33:19 +0200

    Pay normal fee for sweeping paper wallets.

--- wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/SweepWalletFragment.java
@@ -628,6 +628,7 @@ public class SweepWalletFragment extends Fragment
 		setState(State.PREPARATION);
 
 		final SendRequest sendRequest = SendRequest.emptyWallet(application.getWallet().freshReceiveAddress());
+		sendRequest.feePerKb = FeeCategory.NORMAL.feePerKb;
 
 		new SendCoinsOfflineTask(walletToSweep, backgroundHandler)
 		{
