9bd003ba4a39efa37768232630f922a43d3a8ca7
Andreas Schildbach <andreas@schildbach.de>
2015-07-14 13:52:58 +0200

    Adjust fee per KB for the economic and priority fee categories again.

--- wallet/src/de/schildbach/wallet/ui/send/FeeCategory.java
+++ wallet/src/de/schildbach/wallet/ui/send/FeeCategory.java
@@ -27,7 +27,7 @@ public enum FeeCategory
 	/**
 	 * We don't care when it confirms, but it should confirm at some time. Can be days or weeks.
 	 */
-	ECONOMIC(Coin.valueOf(1000)), // 0.01 mBTC
+	ECONOMIC(Coin.valueOf(2000)), // 0.02 mBTC
 
 	/**
 	 * Under normal network conditions, confirms within the next 15 minutes. Can take longer, but this should be an
@@ -38,7 +38,7 @@ public enum FeeCategory
 	/**
 	 * Confirms within the next 15 minutes.
 	 */
-	PRIORITY(Coin.valueOf(20000)); // 0.2 mBTC
+	PRIORITY(Coin.valueOf(50000)); // 0.5 mBTC
 
 	public final Coin feePerKb;
 
