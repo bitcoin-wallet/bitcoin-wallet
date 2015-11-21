dd0c23632402916ac58034bf94dccba6f6dfaa1f
Kalle Rosenbaum <kalle@rosenbaum.se>
2015-11-21 14:12:38 +0100

    Fixed review comments

--- wallet/src/de/schildbach/wallet/ui/pop/SendPopTask.java
+++ wallet/src/de/schildbach/wallet/ui/pop/SendPopTask.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.schildbach.wallet.ui.pop;
 
 import android.os.Handler;
@@ -21,6 +38,9 @@ import se.rosenbaum.jpop.generate.PopGenerationException;
 import se.rosenbaum.jpop.generate.PopGenerator;
 import se.rosenbaum.jpop.generate.PopSigningException;
 
+/**
+ * @author Kalle Rosenbaum
+ */
 public abstract class SendPopTask
 {
 	private static final Logger log = LoggerFactory.getLogger(SendPopTask.class);
