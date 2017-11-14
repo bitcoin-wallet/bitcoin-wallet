8cf4bfc46ab51b353619d1109143aef6b82193ad
Andreas Schildbach <andreas.schildbach@bloq.com>
2017-11-14 10:06:49 +0100

    WalletBalanceFragment: Adjust SOME_BALANCE_THRESHOLD and TOO_MUCH_BALANCE_THRESHOLD.

--- wallet/src/de/schildbach/wallet/ui/WalletBalanceFragment.java
+++ wallet/src/de/schildbach/wallet/ui/WalletBalanceFragment.java
@@ -85,8 +85,8 @@ public final class WalletBalanceFragment extends Fragment {
     private static final int ID_BLOCKCHAIN_STATE_LOADER = 2;
 
     private static final long BLOCKCHAIN_UPTODATE_THRESHOLD_MS = DateUtils.HOUR_IN_MILLIS;
-    private static final Coin SOME_BALANCE_THRESHOLD = Coin.COIN.divide(100);
-    private static final Coin TOO_MUCH_BALANCE_THRESHOLD = Coin.COIN.divide(2);
+    private static final Coin SOME_BALANCE_THRESHOLD = Coin.COIN.divide(200);
+    private static final Coin TOO_MUCH_BALANCE_THRESHOLD = Coin.COIN.divide(4);
 
     @Override
     public void onAttach(final Activity activity) {
