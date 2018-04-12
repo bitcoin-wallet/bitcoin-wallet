55439ee7bf018d0ad9b83f92ba35debf9ea72079
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-04-12 17:08:39 +0200

    ScanActivity: Fix NoSuchMethodError on pre-Android 5.0 (Lollipop) devices.

--- wallet/src/de/schildbach/wallet/ui/scan/ScanActivity.java
+++ wallet/src/de/schildbach/wallet/ui/scan/ScanActivity.java
@@ -88,7 +88,7 @@ public final class ScanActivity extends AbstractWalletActivity
     public static final String INTENT_EXTRA_RESULT = "result";
 
     public static void startForResult(final Activity activity, @Nullable final View clickView, final int requestCode) {
-        if (clickView != null) {
+        if (clickView != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             final int[] clickViewLocation = new int[2];
             clickView.getLocationOnScreen(clickViewLocation);
             final Intent intent = new Intent(activity, ScanActivity.class);
