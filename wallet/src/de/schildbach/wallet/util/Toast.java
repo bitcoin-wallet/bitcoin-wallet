6f9676c8e0ce522aa772b85f1875422a98e51dcd
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-04-13 10:39:18 +0200

    Toast: Add methods for posting toasts to the UI thread.

--- wallet/src/de/schildbach/wallet/util/Toast.java
+++ wallet/src/de/schildbach/wallet/util/Toast.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright the original author or authors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,27 +18,66 @@
 package de.schildbach.wallet.util;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 
 /**
  * @author Andreas Schildbach
  */
 public class Toast {
     private final Context context;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     public Toast(final Context context) {
         this.context = context;
+    }
+
+    public final void postToast(final int textResId, final Object... formatArgs) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                toast(textResId, formatArgs);
+            }
+        });
     }
 
     public final void toast(final int textResId, final Object... formatArgs) {
         customToast(textResId, android.widget.Toast.LENGTH_SHORT, formatArgs);
     }
 
+    public final void postToast(final CharSequence text) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                toast(text);
+            }
+        });
+    }
+
     public final void toast(final CharSequence text) {
         customToast(text, android.widget.Toast.LENGTH_SHORT);
     }
 
+    public final void postLongToast(final int textResId, final Object... formatArgs) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                longToast(textResId, formatArgs);
+            }
+        });
+    }
+
     public final void longToast(final int textResId, final Object... formatArgs) {
         customToast(textResId, android.widget.Toast.LENGTH_LONG, formatArgs);
+    }
+
+    public final void postLongToast(final CharSequence text) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                longToast(text);
+            }
+        });
     }
 
     public final void longToast(final CharSequence text) {
