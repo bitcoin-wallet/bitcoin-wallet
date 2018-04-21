4dacf62f5e685d43c398e9c37cd9ba7ac04e874b
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-04-21 23:56:40 +0200

    ReportIssueDialogFragment: Fix asynchronous loading of wallet.

--- wallet/src/de/schildbach/wallet/ui/ReportIssueDialogFragment.java
+++ wallet/src/de/schildbach/wallet/ui/ReportIssueDialogFragment.java
@@ -173,7 +173,7 @@ public class ReportIssueDialogFragment extends DialogFragment {
         super.onDismiss(dialog);
     }
 
-    private static void appendApplicationInfo(final Appendable report, final WalletApplication application)
+    private void appendApplicationInfo(final Appendable report, final WalletApplication application)
             throws IOException {
         final PackageInfo pi = application.packageInfo();
         final Configuration configuration = application.getConfiguration();
@@ -201,7 +201,7 @@ public class ReportIssueDialogFragment extends DialogFragment {
                 + (lastBackupTime > 0 ? String.format(Locale.US, "%tF %tT %tZ", calendar, calendar, calendar) : "none")
                 + "\n");
         report.append("Network: " + Constants.NETWORK_PARAMETERS.getId() + "\n");
-        final Wallet wallet = application.getWallet();
+        final Wallet wallet = viewModel.wallet.getValue();
         report.append("Encrypted: " + wallet.isEncrypted() + "\n");
         report.append("Keychain size: " + wallet.getKeyChainGroupSize() + "\n");
 
