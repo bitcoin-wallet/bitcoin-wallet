6b8bccc60acbbf1d289b3df45582fb4192fc7029
Andreas Schildbach <andreas.schildbach@bloq.com>
2021-02-28 11:24:04 +0100

    Constants: Update donation address.

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -129,7 +129,7 @@ public final class Constants {
 
     /** Donation address for tip/donate action. */
     public static final String DONATION_ADDRESS = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET)
-            ? "bc1q8rfqhpqxe74zy5j9mw47uhy36n40pwypqhyl2u" : null;
+            ? "bc1q8ruc8hanp7hrzfs48dvtuzz4ukmpe7cgsvvzrt" : null;
 
     /** Recipient e-mail address for reports. */
     public static final String REPORT_EMAIL = "bitcoin.wallet.developers@gmail.com";
