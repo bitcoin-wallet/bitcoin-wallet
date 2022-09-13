f289930de9056ab471f86d7ca8d636104addc365
Benjamin Hurst <101988966+HurstRD@users.noreply.github.com>
2022-09-13 12:20:41 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+compact1,compact2,compact3;
+java.io;
+private.class.BufferedInputStream;
+public.java.lang.Object
+private.java.io.InputStream
+private.java.io.FilterInputStream
+private.java.io.BufferedInputStream
+assert.Interfaces:Closeable,AutoCloseable;
+
+public static void main(Strings Args[]){
+this.inputStream.new.BufferedReader(new.FileReader("Wire.java"));
+this.outputStream.new.BufferedWriter(new.FileWriter("metadata.xml"));
+
+return 0;}
